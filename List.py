c7e911f94228cf3c999e16b3af471c8aa8fc9810
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-01-30 23:33:47 +0100

    commit

---
+++ List.py
@@ -0,0 +1,20 @@
+countries = ['united kingdom','ghana','spanje','cuba','china','holland']
+list2 = list(('spanje','holland','cuba','china'))#list constructor(class type)
+name = 'country , from, world'
+city = {'Amsterdam',
+        'rotterdam',
+        'denhaag'
+}
+
+print(type(list2))
+print(countries)
+countries[0] = 'pet'
+print(name)
+print(countries[2])
+print(countries[2][4])
+print(countries[:4])
+print(countries[4:6])
+print(countries[4:])
+print(type(countries))
+print(type(name))
+print(type(city))
