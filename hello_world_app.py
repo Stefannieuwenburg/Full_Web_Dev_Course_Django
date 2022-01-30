ed7b36d9997bb87affcffe4c70b28682693ec190
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-01-30 21:19:34 +0100

    commit

---
+++ hello_world_app.py
@@ -0,0 +1,5 @@
+from rich import print
+age = 40
+print(f'my name is stefan my age is {age}')
+print(f"hello world",100)
+print(f'welkom')
---
+++ varaiables.py
@@ -0,0 +1,8 @@
+from rich import print
+
+age = 18
+name = 'Tim'
+
+print(f'{name} is a boy')
+print(name+ ' is ', +age)
+print(f'{name} is from turkey')
