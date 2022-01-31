6273956dbed14caaa0c77510fc21a5ad3d278226
Stefan Nieuwenburg <nieuwenburg.stefan@gmail.com>
2022-01-31 16:19:47 +0100

    commit

--- Tuples.py
+++ Tuples.py
@@ -1,15 +1,23 @@
-
+#Only us tuples if you not whant to change things in your tuples list
 three_numbers = (1,2,3,1)
+strings = ('home','land','earth')
+boo = (False,False,True)
 
+# not possibol
+#three_numbers[1] = 23
 
 # print index 1
 print(three_numbers[0])
 
-# type
+# type from div strings/boo/tree_numbers
 print(type(three_numbers))
+
+# type
+print(type(three_numbers[0]))
+
+#length of
+print(len(three_numbers))
 
 
 print(three_numbers)
 
-# not possibile
-three_numbers[1] = 23
